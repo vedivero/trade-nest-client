59fb98f37e13d628e7bd8c9eb4709a1328709aca
vedivero <vedivero@github.com>
2024-12-03 19:06:26 +0900

    Home 페이지 생성, Header 컴포넌트 생성, format number utility 생성 /

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Header from '../components/common/Header';
+
+const Home = () => {
+   return (
+      <>
+         <Header />
+         <Home />
+      </>
+   );
+};
+
+export default Home;
